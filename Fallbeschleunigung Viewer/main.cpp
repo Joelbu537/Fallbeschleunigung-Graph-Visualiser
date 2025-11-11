7304e75fd9d2d8f9e5be631d8d94c8253cad575b
Joelbu <89338010+Joelbu537@users.noreply.github.com>
2025-11-11 20:13:26 +0100

    Projektdateien hinzufügen.

---
+++ Fallbeschleunigung Viewer/main.cpp
@@ -0,0 +1,99 @@
+#include <SFML/Graphics.hpp>
+#include <random>
+#include <cmath>
+#include <iostream>
+
+float xSpan = 0.f;
+float ySpan = 0.f;
+
+sf::Vertex* fillDecoX(float graphMargin, float xInterval, float sizeY, int decoLength) {
+	sf::Vertex* vertexArray = new sf::Vertex[18];
+    for (int i = 0; i < 9; i++)
+    {
+        vertexArray[i * 2 + 0] = sf::Vertex{{ graphMargin + xInterval * (i + 1), sizeY - graphMargin - decoLength }, sf::Color::White, { 0.f, 0.f }};
+        vertexArray[i * 2 + 1] = sf::Vertex{{ graphMargin + xInterval * (i + 1), sizeY - graphMargin + decoLength }, sf::Color::White, { 0.f, 0.f }};
+    }
+	return vertexArray;
+}
+sf::Vertex* fillDecoY(float graphMargin, float yInterval, float sizeY, int decoLength) {
+    sf::Vertex* vertexArray = new sf::Vertex[18];
+    for (int i = 0; i < 9; i++)
+    {
+        vertexArray[i * 2 + 0] = sf::Vertex{ { graphMargin - decoLength, graphMargin + yInterval * (i + 1)}, sf::Color::White, {0.f, 0.f}};
+        vertexArray[i * 2 + 1] = sf::Vertex{ { graphMargin + decoLength, graphMargin + yInterval * (i + 1)}, sf::Color::White, { 0.f, 0.f } };
+    }
+    return vertexArray;
+}
+
+int main(int argc, char* argv[])
+{
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	sf::RenderWindow window(desktop, "Fallbeschleunigung", sf::Style::None);
+    window.setVerticalSyncEnabled(true);
+
+    sf::Clock clock;
+    sf::Vector2u size = window.getSize();
+    std::cout << "Size X: " << size.x << std::endl << "Size Y: " << size.y << std::endl;
+
+	float graphMargin = std::round(size.x / 50.f / 10) * 10; // Runden um Kommazahlen für die Größe der Fläche im Graphen zu vermeiden
+	constexpr int arrowSize = 20;
+	constexpr int decoLength = 20;
+    sf::Vertex lineX[] =
+    {
+        {{ graphMargin - graphMargin / 3, size.y - graphMargin }, sf::Color::White, { 0.f,  0.f }},
+        {{ size.x - graphMargin,   size.y - graphMargin}, sf::Color::White, { 0.f,  0.f }},
+        {{ size.x - graphMargin - arrowSize * 2, size.y - graphMargin - arrowSize}, sf::Color::White, { 0.f, 0.f }},
+		{{ size.x - graphMargin - arrowSize * 2, size.y - graphMargin + arrowSize}, sf::Color::White, { 0.f, 0.f }},
+        {{ size.x - graphMargin, size.y - graphMargin}, sf::Color::White, { 0.f,  0.f }}
+    };
+    sf::Vertex lineY[] =
+    {
+        {{ graphMargin, size.y + graphMargin / 3 - graphMargin }, sf::Color::White, { 0.0f,  0.0f }},
+        {{ graphMargin, graphMargin }, sf::Color::White, { 0.0f,  0.0f }},
+		{{ graphMargin - arrowSize, graphMargin + arrowSize * 2}, sf::Color::White, { 0.0f,  0.0f }},
+        {{ graphMargin + arrowSize, graphMargin + arrowSize * 2}, sf::Color::White, { 0.0f,  0.0f }},
+        {{ graphMargin, graphMargin }, sf::Color::White, { 0.0f,  0.0f }}
+    };
+
+    xSpan = size.x - 2 * graphMargin;
+	ySpan = size.y - 2 * graphMargin;
+	std::cout << "xSpan: " << xSpan << std::endl << "ySpan: " << ySpan << std::endl;
+    float xInterval = xSpan / 10;
+	float yInterval = ySpan / 10;
+
+	sf::Vertex* lineXdeco = fillDecoX(graphMargin, xInterval, size.y, decoLength);
+	sf::Vertex* lineYdeco = fillDecoY(graphMargin, yInterval, size.y, decoLength);
+
+    window.draw(lineX, 5, sf::PrimitiveType::LineStrip);
+    window.draw(lineY, 5, sf::PrimitiveType::LineStrip);
+    window.draw(lineXdeco, 18, sf::PrimitiveType::Lines);
+    window.draw(lineYdeco, 18, sf::PrimitiveType::Lines);
+    window.display();
+
+
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+            else if (const auto* key = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (key->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+            }
+
+        }
+        window.clear();
+        
+		window.draw(lineX, 5, sf::PrimitiveType::LineStrip);
+		window.draw(lineY, 5, sf::PrimitiveType::LineStrip);
+		window.draw(lineXdeco, 18, sf::PrimitiveType::Lines);
+		window.draw(lineYdeco, 18, sf::PrimitiveType::Lines);
+
+        window.display();
+    }
+}
